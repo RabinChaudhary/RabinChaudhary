f2698d4a6a685d0164a9865947c53d0cb15ab286
Rabin Chaudhary <rabinch1011@gmail.com>
2022-05-03 23:04:18 +1000

    footer and contact us div added

---
+++ Project-Canberra/resources/views/inc/footer.blade.php
@@ -0,0 +1,81 @@
+<footer style="background: rgb(112, 14, 112);">
+    <div class="container">
+        <div class="row justify-content-center pt-4">
+            <div class="col">
+                <div class="footer-title">
+                    <h2 class="text-light">SERVICES</h2>
+                </div>
+                <ul class="list-style-none text-light">
+                    <li>> CRM Training and Development</li>
+                    <li>> Cloud Computing Services</li>
+                    <li>> Network Design/ Setup & Security</li>
+                    <li>> SEO and Digital Marketing</li>
+                    <li>> IT Consultation</li>
+                </ul>
+            </div>
+            <div class="col">
+                <div class="footer-title">
+                    <h2 class="text-light">SYDNEY</h2>
+                </div>
+                <ul class="list-style-none text-light">
+
+                    <li>Suite 132 & 133, Level 3 10 Park Road, Hurstville NSW 2220</li>
+                    <li><i class="fa-solid fa-envelope"></i> info@extratechs.com.au</li>
+                    <li><i class="fa-solid fa-mobile-screen"></i> 0411 860 617</li>
+                    <li class="ps-3">0413 490 350</li>
+                    <li class="ps-3">02 9171 7742</li>
+                </ul>
+            </div>
+            <div class="col">
+                <div class="footer-title">
+                    <h2 class="text-light">MELBOURNE</h2>
+                </div>
+                <ul class="list-style-none text-light">
+                    <li>Suite 132 & 133, Level 3 10 Park Road, Hurstville NSW 2220</li>
+                    <li><i class="fa-solid fa-envelope"></i> info@extratechs.com.au</li>
+                    <li><i class="fa-solid fa-mobile-screen"></i> 0411 860 617</li>
+                    <li class="ps-3">0413 490 350</li>
+                    <li class="ps-3">02 9171 7742</li>
+                </ul>
+            </div>
+            <div class="col">
+                <div class="footer-title">
+                    <h2 class="text-light">CANBERRRA</h2>
+                </div>
+                <ul class="list-style-none text-light">
+                    <li>Suite 132 & 133, Level 3 10 Park Road, Hurstville NSW 2220</li>
+                    <li><i class="fa-solid fa-envelope"></i> info@extratechs.com.au</li>
+                    <li><i class="fa-solid fa-mobile-screen"></i> 0411 860 617</li>
+                    <li class="ps-3">0413 490 350</li>
+                    <li class="ps-3">02 9171 7742</li>
+                </ul>
+            </div>
+            <div class="col">
+                <div class="footer-title">
+                    <h2 class="text-light mb-0 pb-0">KATHMANDU</h2>
+                    
+                </div>
+                <ul class="list-style-none text-light">
+                    <li>Suite 132 & 133, Level 3 10 Park Road, Hurstville NSW 2220</li>
+                    <li><i class="fa-solid fa-envelope"></i> info@extratechs.com.au</li>
+                    <li><i class="fa-solid fa-mobile-screen"></i> 0411 860 617</li>
+                    <li class="ps-3">0413 490 350</li>
+                    <li class="ps-3">02 9171 7742</li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+
+    <hr>
+    <div class="footer-bottom ">
+        <div class="container d-flex justify-content-between text-light">
+            <cite>ABN Number : 99 631 450 210</cite>
+            <ul style="display: flex; gap:1em;">
+                <li><i class="fa-brands fa-facebook" style="font-size: 2rem;"></i></li>
+                <li><i class="fa-brands fa-github" style="font-size: 2rem;"></i></li>
+                <li><i class="fa-brands fa-linkedin" style="font-size: 2rem;"></i></li>
+            </ul>
+        </div>
+    </div>
+</footer>
